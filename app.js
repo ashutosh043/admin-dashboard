56d09853548847d798452a193f6397d5947c13f4
ashutosh043 <55331400+ashutosh043@users.noreply.github.com>
2024-10-20 00:04:12 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ const ejs = require('ejs');
 require('./db/conn');
 const fs = require('fs');
 
+// const jwt = require('jsonwebtoken');
 
+//  async function createToken(){
+//     const jwtsecret=    jwt.sign({_id:"670be00901c36e488bdd7190"}, "ashusahmynameisashutoshsahkumarshah", {
+//         expiresIn:"10 second "
+//     });
+//      console.log(jwtsecret);
+
+//      const userVer = await jwt.verify(jwtsecret, 'ashusahmynameisashutoshsahkumarshah');
+//      console.log(userVer);
+//  } 
+
+//  createToken();
 
 const port = process.env.PORT || 8000;
 app.set('view engine','ejs');
@@ -16,17 +28,5 @@ const router = require('./routes/router')
 
 app.use(router)
 
-
-fs.readFile('input.txt', (data, er)=>{
-      if(data) return console.log(data);
-      console.log(er.toString());
-})
-
-let i = 1;
-while (i <=5) {
-  console.log("The number is " + i);
-  i++;
-}
- 
 app.listen(port);
 
