c25a1254386591f040ac773660b1b45d188e5e9c
ashutosh043 <55331400+ashutosh043@users.noreply.github.com>
2024-10-20 00:42:10 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,21 +2,7 @@ const express = require('express');
 const app =  express();
 const ejs = require('ejs');
 require('./db/conn');
-const fs = require('fs');
 
-// const jwt = require('jsonwebtoken');
-
-//  async function createToken(){
-//     const jwtsecret=    jwt.sign({_id:"670be00901c36e488bdd7190"}, "ashusahmynameisashutoshsahkumarshah", {
-//         expiresIn:"10 second "
-//     });
-//      console.log(jwtsecret);
-
-//      const userVer = await jwt.verify(jwtsecret, 'ashusahmynameisashutoshsahkumarshah');
-//      console.log(userVer);
-//  } 
-
-//  createToken();
 
 const port = process.env.PORT || 8000;
 app.set('view engine','ejs');
